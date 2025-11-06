bc72518b5cbee3b2c8fc6f0b6b819239ecd0dd0d
Nikita <nick.data.k@icloud.com>
2025-11-06 20:13:41 +0100

    Implement equals and hashCode methods in Position

--- Position.java
+++ Position.java
@@ -24,7 +24,18 @@ public class Position {
     public String toString() {
         return "(" + x + "," + y + ")";
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Position position = (Position) obj;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(x, y);
+    }
     
 }
-
-
